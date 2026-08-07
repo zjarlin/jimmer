60476acbb1a06fd49091b5c74bbd1d2b2845a7e8
zjarlin <zjarlin@outlook.com>
2026-08-07 11:58:40 +0800

    refactor(build): source LSI from submodule

--- project/settings.gradle.kts
+++ project/settings.gradle.kts
@@ -27,6 +27,26 @@ include(
     "jimmer-sql-test:jimmer-sql-test-support",
 )
 
+val lsiRootDirectory = file("../lib/lsi")
+require(lsiRootDirectory.isDirectory) {
+    "LSI submodule is missing. Run 'git submodule update --init --recursive'."
+}
+listOf(
+    "lsi-core",
+    "lsi-apt",
+    "lsi-ksp",
+    "lsi-poet",
+    "lsi-poet-javapoet",
+    "lsi-poet-kotlinpoet",
+    "lsi-jimmer",
+).forEach { moduleName ->
+    val moduleDirectory = lsiRootDirectory.resolve(moduleName)
+    require(moduleDirectory.resolve("build.gradle.kts").isFile) {
+        "LSI submodule module '$moduleName' is missing. Run 'git submodule update --init --recursive'."
+    }
+    project(":$moduleName").projectDir = moduleDirectory
+}
+
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 dependencyResolutionManagement {
